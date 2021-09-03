b2ab40f87e457c8a648413b5fd0fd76d8968dd4e
Devon Gregory <gregordevo@ecs.vuw.ac.nz>
2021-09-03 19:43:29 +1200

    added infrastructure for messaging a certain member from their route

--- Components/Screens/MessagesScreen.js
+++ Components/Screens/MessagesScreen.js
@@ -34,20 +34,6 @@ const Messages = () =>
     {
         const [messages, setMessages] = useState([]);
 
-        // useEffect(() => {
-        //     setMessages([
-        //         {
-        //             _id: 1,
-        //             text: 'Hello developer',
-        //             createdAt: new Date(),
-        //             user: {
-        //                 _id: 2,
-        //                 name: 'React Native',
-        //                 avatar: 'https://placeimg.com/140/140/any',
-        //             },
-        //         },
-        //     ])
-        // }, [])
 
         useLayoutEffect(() => {
                const unsubscribe = db
